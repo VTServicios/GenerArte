fa4701c40aa06384c035a1162d4532ab13007145
VTServicios <125997425+VTServicios@users.noreply.github.com>
2024-04-27 09:00:56 -0300

    comentario

--- 12/12.sh
+++ 12/12.sh
@@ -487,7 +487,6 @@ create_uefi_iso() {
 	echo "Listo."
 	echo
 }
-# ----------------------------QUINIENTOS-----------
 # Ejecutar según los modificadores
 #
 if [ "$EJECUTAR" == "limpiar" ]; then
@@ -524,7 +523,7 @@ if [ "$EJECUTAR" == "cambios" ]; then
 fi
 
 if [ "$EJECUTAR" == "boot" ]; then
-	# Rebuild existing ISO image (update bootloader)
+	# ReGenerando ISO image existente (update bootloader)
 	create_iso
 fi
 # --------FIN
